8ac97fe9111c6a7361534f4ee7681b89c385c9f6
Vinicius Pacheco <vfpweb@gmail.com>
2018-12-16 03:03:27 -0300

    Created producer decorator

--- tutorial/producer/producer.py
+++ tutorial/producer/producer.py
@@ -1,16 +1,50 @@
 from kafka import KafkaProducer
-import random
+from rx import Observer, Observable
 import json
+import functools
 
 
-producer = KafkaProducer(
-    bootstrap_servers=['localhost:29092'],
-    value_serializer=lambda m: json.dumps(m).encode('ascii'),
-)
+class Producer(Observer):
+    def __init__(self, message_sender, compensatory_request=None):
+        self.message_sender = message_sender
+        self.compensatory_request = compensatory_request
+        self.producer = KafkaProducer(
+            bootstrap_servers=['localhost:29092'],
+            value_serializer=lambda m: json.dumps(m).encode('ascii'),
+        )
 
-print('Ctrl+c to stop')
-while True:
-    producer.send(
-        'kafka-python-topic',
-        random.randint(1, 999),
-    )
+    def on_next(self, value):
+        self.producer.send(
+            self.message_sender,
+            value,
+        )
+
+    def on_completed(self):
+        self.producer.close()
+        print('Done!!!')
+
+    def on_error(self, error):
+        if self.compensatory_request is None:
+            print('Error raised: {}'.format(error))
+        else:
+            print('Error non-compensatory_request: {}'.format(error))
+
+
+def producer_rx(message_sender, compensatory_request=None):
+    def decorator_rx(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            source = Observable.of(func(*args, **kwargs))
+            source.subscribe(Producer(message_sender))
+        return wrapper
+    return decorator_rx
+
+
+@producer_rx(message_sender='kafka-python-topic')
+def my_func(name):
+    return {'name': name}
+
+
+if __name__ == '__main__':
+    print('Ctrl+c to stop')
+    my_func('Pacheco')
