998312a632a891dea20804922251e7f453d7ec66
Vinicius Pacheco <vfpweb@gmail.com>
2019-01-19 01:31:02 -0300

    Removed time.sleep

--- KafkaWithRX/consumer.py
+++ KafkaWithRX/consumer.py
@@ -42,6 +42,4 @@ class MyImplementation(RXConsumer):
     consumer_topic = 'kafka-python-topic'
 
     def process_consumer(self, msg):
-        import time
-        time.sleep(10)
         print('Received {}'.format(msg))
